3ac85647483b87005012a88d7295b90c9e20b6b0
Przemek <przemek.zielinski01@gmail.com>
2022-03-14 19:41:40 +0100

    mapped list items with state

--- src/Components/ListItem.js
+++ src/Components/ListItem.js
@@ -1,40 +1,72 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import {CartContext} from '../Context/CartContext';
-import pizzaPepperoni from '../img/pizza-pepperoni.png'
+import { CartContext } from "../Context/CartContext";
+import pizzaPepperoni from "../img/pizza-pepperoni.png";
+import pizzaHawajska from "../img/pizza-hawajska.png";
 import "./ListItem.css";
 
+function ListItem() {
+  const [menu, setMenu] = useState([
+    {
+      name: "Pizza Pepperoni",
+      price: 29.99,
+      image: pizzaPepperoni,
+      qty: 0,
+    },
+    {
+      name: "Pizza Hawajska",
+      price: 30.99,
+      image: pizzaHawajska,
+      qty: 0,
+    },
+  ]);
 
-function ListItem()  {
-  const {qty, setQty, setCartItem, cartQty, setCartQty} = useContext(CartContext);
-  const menuItemTitle = "Pizza Pepperoni";
-  const addToCart = () => {
-    if (qty >= 1) {
-      setCartItem(`${menuItemTitle}`);
-      setCartQty(cartQty + qty);
-      setQty(0);
+  const addToCart = <button className="button-add">Add</button>;
+
+  const substraction = (i) => {
+    if (menu[i].qty > 0) {
+      let prevState = [...menu];
+      let prevElement = { ...prevState[i] };
+      prevElement.qty--;
+      prevState[i] = prevElement;
+      setMenu(prevState);
     }
   };
 
-  const addButton = <button className="button-add" onClick={() => addToCart()}>Add</button>
+  const addition = (i) => {
+    let prevState = [...menu];
+    let prevElement = { ...prevState[i] };
+    prevElement.qty++;
+    prevState[i] = prevElement;
+    setMenu(prevState);
+  };
 
   return (
     <>
-      <div className="list-item d-flex justify-content-between align-items-center">
-        <img src={pizzaPepperoni} alt="pizza pepperoni" />
-        <div>
-          <h2>{menuItemTitle}</h2>
-          <h3>29.99zł</h3>
-        </div>
-        <div className="d-flex flex-column justify-content-between">
-          <div className="d-flex">
-          <button className="button-qty" onClick={() => setQty(qty - 1)}>-</button>
-          <div>{qty}</div>
-          <button className="button-qty" onClick={() => setQty(qty + 1)}>+</button>
+      {menu.map((menuItem, i) => (
+        <div
+          key={i}
+          className="list-item d-flex justify-content-between align-items-center"
+        >
+          <img src={menuItem.image} alt={menuItem.name} />
+          <div>
+            <h2>{menuItem.name}</h2>
+            <h3>{menuItem.price} zł</h3>
           </div>
-          {qty >=1 ? addButton : null}
+          <div className="d-flex flex-column justify-content-between">
+            <div className="d-flex">
+              <button className="button-qty" onClick={() => substraction(i)}>
+                -
+              </button>
+              <div>{menuItem.qty}</div>
+              <button className="button-qty" onClick={() => addition(i)}>
+                +
+              </button>
+            </div>
+            {menuItem.qty >= 1 ? addToCart : null}
+          </div>
         </div>
-      </div>
+      ))}
     </>
   );
 }
