ff7c500d7f27151341c35d3608f1903193180ab7
Przemek <przemek.zielinski01@gmail.com>
2022-03-21 23:25:13 +0100

    cart: checkout btn condition

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -6,9 +6,9 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 
-
 function Cart() {
-  const { menu, setMenu, notyfication, setNotyfication } = useContext(CartContext);
+  const { menu, setMenu, notyfication, setNotyfication } =
+    useContext(CartContext);
 
   //Total amount
   let sum = 0;
@@ -44,54 +44,67 @@ function Cart() {
 
   const clearCartItem = (i) => {
     let prevState = [...menu];
-    let prevElement = { ...prevState[i] }
-    let prevNotyfication = notyfication - prevElement.cartQty ;
-    setNotyfication(prevNotyfication)
-    prevElement.cartQty = 0
+    let prevElement = { ...prevState[i] };
+    let prevNotyfication = notyfication - prevElement.cartQty;
+    setNotyfication(prevNotyfication);
+    prevElement.cartQty = 0;
     prevState[i] = prevElement;
     setMenu(prevState);
-  }
+  };
 
-  const renderCheck = notyfication
+  const renderCheck = notyfication;
 
   const cartItemsList = () => {
-    return  <>{menu.map((cartItem, i) => (
-      <div key={cartItem.name}>
-        {cartItem.cartQty > 0 ? (
-          <div className="d-flex justify-content-between align-items-center cart-item">
-            <p>{`${cartItem.name} x${cartItem.cartQty}`}</p>
-            <p>{`${Math.floor(cartItem.price * cartItem.cartQty * 100) / 100} zł`}</p>
-            <div className="d-flex flex-row">
-              <button className="cart-btn" onClick={() => substraction(i)}>
-                -
-              </button>
-              <button className="cart-btn" onClick={() => addition(i)}>
-                +
-              </button>
-              <div className="icon-bg cart-btn d-flex justify-content-center align-items-center" onClick={() => clearCartItem(i)}>
-                <FontAwesomeIcon icon={faTrash} className="cart-icon" />
+    return (
+      <>
+        {menu.map((cartItem, i) => (
+          <div key={cartItem.name}>
+            {cartItem.cartQty > 0 ? (
+              <div className="d-flex justify-content-between align-items-center cart-item">
+                <p>{`${cartItem.name} x${cartItem.cartQty}`}</p>
+                <p>{`${
+                  Math.floor(cartItem.price * cartItem.cartQty * 100) / 100
+                } zł`}</p>
+                <div className="d-flex flex-row">
+                  <button className="cart-btn" onClick={() => substraction(i)}>
+                    -
+                  </button>
+                  <button className="cart-btn" onClick={() => addition(i)}>
+                    +
+                  </button>
+                  <div
+                    className="icon-bg cart-btn d-flex justify-content-center align-items-center"
+                    onClick={() => clearCartItem(i)}
+                  >
+                    <FontAwesomeIcon icon={faTrash} className="cart-icon" />
+                  </div>
+                </div>
               </div>
-            </div>
+            ) : null}
           </div>
-        ) : null}
-      </div>
-    ))}
-    <div className="divider"></div>
-    <p className="summary">{summary()}</p>
-
-    </>
-  }
+        ))}
+        <div className="divider"></div>
+        <p className="summary">{summary()}</p>
+      </>
+    );
+  };
 
   return (
     <>
       <h1 className="title">Cart</h1>
-     {renderCheck > 0 ? cartItemsList() : <p>Add something :)</p>}
-     <Link to="/">
-        <button className="button-add cart-btn-width-fix text-uppercase">Back</button>
+      {renderCheck > 0 ? cartItemsList() : <p>Add something :)</p>}
+      <Link to="/">
+        <button className="button-add cart-btn-width-fix text-uppercase">
+          Back
+        </button>
       </Link>
-      <Link to="/checkout">
-        <button className="button-add cart-btn-width-fix text-uppercase">Checkout</button>
-      </Link>
+      {renderCheck > 0 ? (
+        <Link to="/checkout">
+          <button className="button-add cart-btn-width-fix text-uppercase">
+            Checkout
+          </button>
+        </Link>
+      ) : null}
     </>
   );
 }
